44996f613f346d7c5d795ab461eb3a88fdb9224e
constantino <muheec@gmail.com>
2020-04-30 12:52:38 -0700

    init

---
+++ wiki/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from wiki.models import Page
+
+
+class PageCreateForm(forms.ModelForm):
+    """ Render and process a form based on the Page model. """
+    class Meta:
+     model = Page
+     fields = [
+         'title',
+         'content',
+         'author'
+     ]
---
+++ wiki/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.views import generic
+from django.urls import reverse_lazy
+
+from wiki.models import Page
+from .forms import PageCreateForm
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class PageListView(ListView):
+    """ Renders a list of all Pages. """
+    model = Page
+
+    def get(self, request):
+        """ GET a list of Pages. """
+        pages = self.get_queryset().all()
+        return render(request, 'list.html', {
+          'pages': pages
+        })
+
+class PageDetailView(DetailView):
+  """ Renders a specific page based on it's slug."""
+  model = Page
+
+  def get(self, request, slug):
+      """ Returns a specific wiki page by slug. """
+      page = self.get_queryset().get(slug__iexact=slug)
+      return render(request, 'page.html', {
+        'page': page
+      })
+
+class PageCreateView(CreateView):
+  def get(self, request, *args, **kwargs):
+      context = {'form': PageCreateForm()}
+      return render(request, 'new.html', context)
+      
+  def post(self, request, *args, **kwargs):
+    form = PageCreateForm(request.POST)
+    if form.is_valid():
+        page = form.save()
+        return HttpResponseRedirect(reverse_lazy('wiki-details-page', args=[page.slug]))
+    return render(request, 'new.html', {'form': form})
